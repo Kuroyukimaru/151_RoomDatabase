8941624f9822155094da6b00f41a93617d5d8c65
“Kuroyukimaru” <oranggabut712@gmail.com>
2025-11-28 09:48:30 +0700

    Untuk menambahkan data siswa baru ke database

--- app/src/main/java/com/example/roomdatabase/room/SiswaDao.kt
+++ app/src/main/java/com/example/roomdatabase/room/SiswaDao.kt
@@ -9,5 +9,7 @@ import kotlinx.coroutines.flow.Flow
 interface SiswaDao {
     @Query(value = "SELECT * FROM tblSiswa ORDER BY nama ASC")
     fun getAllSiswa(): Flow<List<Siswa>>
-    
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(siswa: Siswa)
 }
