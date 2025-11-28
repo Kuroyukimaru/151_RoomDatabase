1fcf4b00984fca4d2d1b628de359816798d8b694
“Kuroyukimaru” <oranggabut712@gmail.com>
2025-11-28 10:14:57 +0700

    importnya

--- app/src/main/java/com/example/roomdatabase/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/roomdatabase/repositori/ContainerApp.kt
@@ -1,5 +1,8 @@
 package com.example.roomdatabase.repositori
 
+import android.content.Context
+import com.example.roomdatabase.room.DatabaseSiswa
+
 
 interface ContainerApp {
     val repositoriSiswa : RepositoriSiswa
