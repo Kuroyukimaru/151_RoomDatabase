e87bf8e877521d5ad39b7fccd44fdf80d44fac70
“Kuroyukimaru” <oranggabut712@gmail.com>
2025-11-28 10:14:37 +0700

    penyedia dependency yang membuat dan menyimpan RepositoriSiswa agar bisa dipakai oleh ViewModel atau UI di seluruh aplikasi.

--- app/src/main/java/com/example/roomdatabase/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/roomdatabase/repositori/ContainerApp.kt
@@ -6,3 +6,12 @@ interface ContainerApp {
 }
 
 // 2. Concrete Implementation of the Container
+class ContainerDataApp(private val context: Context):
+    ContainerApp {
+
+    override val repositoriSiswa: RepositoriSiswa by lazy {
+        OfflineRepositoriSiswa(
+            siswaDao = DatabaseSiswa.getDatabase(context).siswaDao()
+        )
+    }
+}
