d24503486192d0afb5b74edd21d6e59dfab28ed5
“Kuroyukimaru” <oranggabut712@gmail.com>
2025-11-28 13:17:19 +0700

    ssan

--- app/src/main/java/com/example/roomdatabase/room/SiswaDao.kt
+++ app/src/main/java/com/example/roomdatabase/room/SiswaDao.kt
@@ -6,6 +6,7 @@ import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import kotlinx.coroutines.flow.Flow
 
+@Dao
 interface SiswaDao {
     @Query(value = "SELECT * FROM tblSiswa ORDER BY nama ASC")
     fun getAllSiswa(): Flow<List<Siswa>>
---
+++ ssan/ssan.md
@@ -0,0 +1,3 @@
+![Belom ada Data](belombuatdata.png)
+![Udah ada Data](udahadadata.png)
+![Tampilan Tambah Siswa](untuknambahsiswa.png)
