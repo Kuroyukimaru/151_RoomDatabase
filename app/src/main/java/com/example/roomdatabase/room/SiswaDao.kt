869ac53b9f257fdf94ac1cb900c8a9546fb87bfd
“Kuroyukimaru” <oranggabut712@gmail.com>
2025-11-28 09:48:03 +0700

    Untuk mengambil semua data siswa dari tabel tblSiswa secara real-time

---
+++ app/src/main/java/com/example/roomdatabase/room/SiswaDao.kt
@@ -0,0 +1,13 @@
+package com.example.roomdatabase.room
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+interface SiswaDao {
+    @Query(value = "SELECT * FROM tblSiswa ORDER BY nama ASC")
+    fun getAllSiswa(): Flow<List<Siswa>>
+    
+}
