12e508362b6f1029f72a8b499edec842c1f4dadd
“Kuroyukimaru” <oranggabut712@gmail.com>
2025-11-28 11:30:13 +0700

    status UI

---
+++ app/src/main/java/com/example/roomdatabase/viewmodel/EntryViewModel.kt
@@ -0,0 +1,18 @@
+package com.example.roomdatabase.viewmodel
+
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.roomdatabase.repositori.RepositoriSiswa
+import com.example.roomdatabase.room.Siswa
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class EntryViewModel(private val repositoriSiswa: RepositoriSiswa) : ViewModel() {
+    
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
